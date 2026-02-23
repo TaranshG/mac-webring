d9bf34024627591f12e77cd6ea34462cf6447905
Taransh Goyal <taransh.goyal@gmail.com>
2026-02-22 23:10:23 -0500

    Pull request for adding myself to the webring :)

--- js/data.js
+++ js/data.js
@@ -7,6 +7,11 @@ const webringData = {
             "year": "2028",
             "website": "https://anitajiang.me"
         }
+        {
+            "name": "Taransh Goyal",
+            "year": "2029",
+            "website": "https://www.linkedin.com/in/taransh-goyal-6a52b8296/"
+        },
         // add new sites above this comment
         // for clarity: year refers to your projected graduation year.
         // follow this format:
